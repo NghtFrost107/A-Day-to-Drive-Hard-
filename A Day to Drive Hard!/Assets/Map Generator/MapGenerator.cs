466560bb35d2cf35b4371a6cad69195e233d78db
Mitchell <mitch.mcd18@gmail.com>
2018-08-23 17:43:17 +1200

    Random Generation Started
    Initial code for the random generation. Currently just generates blocks that are either terrain or nothing, in no order

--- A Day to Drive Hard!/Assets/Map Generator/MapGenerator.cs
+++ A Day to Drive Hard!/Assets/Map Generator/MapGenerator.cs
@@ -5,9 +5,69 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour {
 
     int height = Screen.height;
-	
-	// Update is called once per frame
-	void Update () {
-		
+    int width = Screen.width;
+
+    int[,] section;
+
+    string seed;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
 	}
+
+    void GenerateMap()
+    {
+        section = new int[width, height];
+        RandomVoidFill();
+
+    }
+
+    void RandomVoidFill()
+    {
+        System.Random random = new System.Random();
+
+        for (int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1)
+                {
+                    section[x, y] = 1;
+                }
+                else
+                {
+                    section[x, y] = random.Next(0, 100) > 50 ? 1 : 0;
+                }
+            }
+        }
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        if (section != null)
+        {
+            Gizmos.color = Color.green;
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (section[x, y] == 1)
+                    {
+                        Vector3 pos = new Vector3(-width / 2 + x, 0, -height / 2 + y);
+                        Gizmos.DrawCube(pos, Vector3.one);
+                    }
+
+                }
+            }
+        }
+    }
 }
