8cdd342893c6d3fe76fbad8dd7a6e019ee619180
Mitchell <mitch.mcd18@gmail.com>
2018-08-23 15:33:03 +1200

    Created Player Files
    Showing the team how to push/pull files to GitHub

--- A Day to Drive Hard!/Assets/Player/PlayerController.cs
+++ A Day to Drive Hard!/Assets/Player/PlayerController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGenerator : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
-    int height = Screen.height;
+	// Use this for initialization
+	void Start () {
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
