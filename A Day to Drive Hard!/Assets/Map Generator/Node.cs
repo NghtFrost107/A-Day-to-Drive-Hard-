350f207982c8c10ee0d53a2b0da55e7e40e1581a
Mitchell <mitch.mcd18@gmail.com>
2018-09-03 17:03:10 +1200

    Readding Files

---
+++ A Day to Drive Hard!/Assets/Map Generator/Node.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    public Vector3 position;
+    public int verticeIndex = -1;
+
+    public Node(Vector3 pos)
+    {
+        position = pos;
+    }
+}
+
+public class MainNode : Node
+{
+    public bool active;
+    public Node above, right;
+
+    public MainNode(Vector3 _pos, bool _active) : base(_pos)
+    {
+        active = _active;
+        above = new Node(position + Vector3.forward);
+        right = new Node(position + Vector3.right);
+    }
+}
+
+
---
+++ A Day to Drive Hard!/Assets/Map Generator/Square.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum StateTypes
+{
+    AIR, TERRAIN, BOUNDARY
+}
+
+public class Square {
+
+    public MainNode topLeft, topRight, bottomRight, bottomLeft;
+    public int configuration;
+    /*
+     * Here holds the state for the current square.
+     * 
+     * State Types:
+     * 0 = Nothing (Is air)
+     * 1 = Terrain
+     * 2 = Boundary Terrain (Between normal Terrain and Air)
+     */
+    public int state;
+
+    public int x; //Coordinates from start of section generated (Bottom Left = 0,0)
+    public int y;
+
+    public Square (int cordX, int cordY)
+    {
+        x = cordX;
+        y = cordY;
+    }
+
+    public Square(int cordX, int cordY, int state) : this(cordX, cordY)
+    { 
+        this.state = state;
+    }
+
+    public void AssignNodes(MainNode topLeft, MainNode topRight, MainNode bottomRight, MainNode bottomLeft)
+    {
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
