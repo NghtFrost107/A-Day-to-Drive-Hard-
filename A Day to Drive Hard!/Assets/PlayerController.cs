e9677d18d24fe1b4a691e11b147327bdfa73ecfd
Ravmyster1121 <rhninety@gmail.com>
2018-09-06 09:41:17 +1200

    Added Obstacle Object
    Created a basic obstacle prefab and set up collision detection for obstacles and the coin pickups

---
+++ A Day to Drive Hard!/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Player Collided with an obstacle");
+        }
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            Debug.Log("Player Collided with a pickup");
+        }
+    }
+}
