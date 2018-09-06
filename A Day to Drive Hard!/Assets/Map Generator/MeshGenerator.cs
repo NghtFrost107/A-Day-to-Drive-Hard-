7cef24c2cbcbdedb56a89d17012b788a0accf142
Mitchell <mitch.mcd18@gmail.com>
2018-09-06 22:33:17 +1200

    Added Comments
    Added comments to the mesh generator file to explain how the curves are created and the process the code uses to determine the type of curve to generate

--- A Day to Drive Hard!/Assets/Map Generator/MeshGenerator.cs
+++ A Day to Drive Hard!/Assets/Map Generator/MeshGenerator.cs
@@ -19,7 +19,17 @@ public class MeshGenerator : MonoBehaviour {
 
         Square[] boundarySquares = GetComponent<MapGenerator>().boundarySquares;
 
-        //Runs through all boundary squares in the section of the map
+        /* Runs through all boundary squares in the section of the map
+         * 
+         * It is reponsible for assigning the meshes to these squares and linking them together smoothly with curved meshes.
+         * 
+         * Comments are shown above each if statement explaining the current square it is looking at and how it uses the squares around it to determine how to link the squares smoothly.
+         * 
+         * P = Previous Square
+         * x = Current Square
+         * N = Next Square
+         * S = Next Next Square
+         */
         for (int x = 1; x < boundarySquares.Length - 2; x++)
         {
             Square previousSquare = GetComponent<MapGenerator>().FindBoundarySquare(x - 1); 
@@ -29,31 +39,46 @@ public class MeshGenerator : MonoBehaviour {
             {
                 SectionCollider sectionCollider = GetComponent<SectionCollider>();
                 Vector2[] collisionPoints;
-                if (nextSquare.y > boundarySquares[x].y) //When the elevation is going up
+
+                /*    _
+                 *  _|N|
+                 * |x|^
+                 * 
+                 * When the elevation is going up
+                 */
+                if (nextSquare.y > boundarySquares[x].y)
                 {
-                    AssignSquareMesh(boundarySquares[x]);
-                    
-                    if (nextNextSquare.y > nextSquare.y) //When the elevation will still be going up upon next iteration
+                    AssignSquareMesh(boundarySquares[x]); //Creates the mesh for the current square
+
+                    /*       _
+                     *     _|S|
+                     *   _|N|^
+                     *  |x|
+                     * When the elevation will still be going up for the next 2 squares
+                     */
+                    if (nextNextSquare.y > nextSquare.y)
                     {
-                        /**       _
-                         *      _|N|
+                        /*        _
+                         *      _|S|
                          *    _|N|
                          *  _|x| -- You are here (x)
                          * |P|
-                         * 
                          * Is currently going up a hill (Previous squares was lower and next squares are higher than the current square)
+                         * Will create a straight diagonal line from the current square (x) to the next square (N)
+                         * 
                          */
                         if (previousSquare.y < boundarySquares[x].y)
                         {
                             AssignTriangleMesh(boundarySquares[x].topLeft.position, nextSquare.topLeft.position, boundarySquares[x].topRight.position);
                             collisionPoints = new Vector2[2] { boundarySquares[x].topLeft.position, nextSquare.topLeft.position };
                         }
-                        /**        _
-                         *       _|N|
+                        /*         _
+                         *       _|S|
                          *  _  _|N|
                          * |P||x| -- You are here (x)
                          * 
                          * At the bottom of a hill starting to go up (First square to change elevation from flat terrain)
+                         * Will curve the mesh from the start of the previous square (P) to the end of the current square (x).
                          */
                         else 
                         {
@@ -61,48 +86,124 @@ public class MeshGenerator : MonoBehaviour {
                             AssignCurveVertices(collisionPoints, nextSquare.bottomLeft.position);
                         }
                     }
+                     /*    _  _
+                     *   _|N||S|
+                     *  |x|^ 
+                     * When the elevation will only be going up for the next square
+                     */
                     else
                     {
-                        if (previousSquare.y < boundarySquares[x].y) //Is at the end of a hill (Previous square was lower but the next square goes back to flat terrain)
+                        /*       _  _
+                         *     _|N||S|
+                         *   _|x| -- You are here (x)
+                         *  |P|
+                         *  
+                         *  Is at the end of a hill (Previous square was lower but the next square goes back to flat terrain)
+                         *  Will curve the mesh from the current square (x) to the end of the next square (N)
+                         */
+                        if (previousSquare.y < boundarySquares[x].y)
                         {
                             collisionPoints = calculateQuadraticCurve(boundarySquares[x].topLeft, new Vector2(nextSquare.topLeft.position.x - 0.25f, nextSquare.topLeft.position.y), nextSquare.topRight);
                             AssignCurveVertices(collisionPoints, nextSquare.bottomRight.position);
                             x++; //Skip over next boundary square as mesh has already been drawn for it
                         }
-                        else { //When a small elevation change occurs that doesn't match the other criteria (Normally occurs if elevation changes by 1 square but rest is flat terrain)
-                            collisionPoints = calculateCubicCurve(previousSquare.topLeft, new Vector2(previousSquare.topLeft.position.x + 1, previousSquare.topRight.position.y), new Vector2(nextSquare.topLeft.position.x - 1, nextSquare.topLeft.position.y), nextSquare.topLeft);
-                            AssignCurveVertices(collisionPoints, nextSquare.bottomLeft.position);
+                        /*
+                         * When a small elevation change occurs that doesn't match the other criteria (Normally occurs if elevation changes by 1 square but rest is flat terrain)
+                         * 
+                         * Example:
+                         *        _  _
+                         *   _  _|N||S|
+                         *  |P||x| -- You are here(x)
+                         *  
+                         *  Will curve the mesh from the start of the current square (x) to the end of the next square (N)
+                         */
+                        else
+                        { 
+                            collisionPoints = calculateCubicCurve(boundarySquares[x].topLeft, new Vector2(boundarySquares[x].topLeft.position.x + 1, boundarySquares[x].topRight.position.y), new Vector2(nextSquare.topRight.position.x - 1, nextSquare.topRight.position.y), nextSquare.topRight);
+                            AssignCurveVertices(collisionPoints, nextSquare.bottomRight.position);
+                            x++;
                         }
                         
                     }
 
                     sectionCollider.addPoint(collisionPoints);
                 }
+                /*  _
+                 * |x|_
+                 *  >|N|
+                 *   
+                 * When the elevation is going down
+                 */
                 else if (nextSquare.y < boundarySquares[x].y)
-                {                    
+                {
+                    /*  _
+                     * |x|_
+                     *   |N|_
+                     *    >|S|
+                     * When the elevation will still be going down for the next 2 squares
+                     */
                     if (nextNextSquare.y < nextSquare.y)
                     {
-                        
+                        /*  _
+                         * |P|_
+                         *   |x|_
+                         *     |N|_
+                         *       |S|
+                         * Is currently going down a hill (Previous squares was higher and next squares are lower than the current square)
+                         * Will create a straight diagonal line from the current square (x) to the next square (N)
+                         * 
+                         */
                         if (previousSquare.y > boundarySquares[x].y)
                         {
                             AssignSquareMesh(boundarySquares[x]);
                             AssignTriangleMesh(boundarySquares[x].topRight.position, nextSquare.topRight.position, boundarySquares[x].bottomRight.position);
                             collisionPoints = new Vector2[2] { boundarySquares[x].topRight.position, nextSquare.topRight.position };
                         }
+                        /*  _  _
+                         * |P||x|_
+                         *      |N|_
+                         *        |S|
+                         * 
+                         * At the top of a hill starting to go down (First square to change elevation from flat terrain)
+                         * Will curve the mesh from the start of the current square (x) to the end of the next square (N).
+                         */
                         else
                         {
                             collisionPoints = calculateQuadraticCurve(boundarySquares[x].topLeft, new Vector2(boundarySquares[x].topRight.position.x, boundarySquares[x].topLeft.position.y), nextSquare.topRight);
                             AssignCurveVertices(collisionPoints, boundarySquares[x].bottomLeft.position);
                         }
                     }
+                    /*  _
+                     * |x|_  _
+                     *  >|N||S|
+                     *  
+                     * When the elevation will only be going down for the next square
+                     */
                     else
                     {
+                        /*   _
+                         *  |P|_
+                         *    |x|_  _
+                         *      |N||S|
+                         *  
+                         *  Is at the end of a hill (Previous square was higher but the next square goes back to flat terrain)
+                         *  Will curve the mesh from the end of current square (x) to the end of the next next square (S)
+                         */
                         if (previousSquare.y > boundarySquares[x].y)
                         {
                             AssignSquareMesh(boundarySquares[x]);
                             collisionPoints = calculateQuadraticCurve(boundarySquares[x].topRight, new Vector2(nextSquare.topRight.position.x - 0.25f, nextSquare.topRight.position.y), nextNextSquare.topRight);
                             AssignCurveVertices(collisionPoints, boundarySquares[x].bottomRight.position);
                         }
+                        /*
+                        * When a small elevation change occurs that doesn't match the other criteria (Normally occurs if elevation changes by 1 square but rest is flat terrain)
+                        * 
+                        * Example:
+                        *   _  _
+                        *  |P||x|_  _
+                        *       |N||S|
+                        *  Will curve the mesh from the start of the current square (x) to the end of the next square (N)
+                        */
                         else
                         {
                             AssignSquareMesh(boundarySquares[x + 1]);
@@ -114,40 +215,23 @@ public class MeshGenerator : MonoBehaviour {
 
                     sectionCollider.addPoint(collisionPoints);
                 }
+                /*
+                 * When the elevation remains the same for the next square
+                 *     _  _
+                 * ...|x||N|...
+                 */
                 else if (nextSquare.y == boundarySquares[x].y)
                 {
                     AssignSquareMesh(boundarySquares[x]);
                     sectionCollider.addPoint(boundarySquares[x].topRight.position);
                 }
             }
-        }/*
-        for (int x = 0; x < section.GetLength(0); x++)
-        {
-            for (int y = 0; y < section.GetLength(1); y++)
-            {
-                if (section[x, y].y > lowestPoint)
-                {
-                    if (section[x, y].state != 0)
-                    {
-                        //AssignSquareMesh(section[x, y]);
-                    }
-                    
-
-                    if (section[x, y].state == 2 && x < section.GetLength(0) - 2) //If the square is a boundary square and is not within the last 2 squares at the end of the section
-                    {
-
-                       
-                    }
-                }
-            }*/
-
-              
-
+        }
 
         Mesh mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        mesh.vertices = vertices.ToArray();
+        mesh.vertices = vertices.ToArray(); //Assigns the vertices and triangles to the mesh to be generated
         mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals();
     }
@@ -203,18 +287,6 @@ public class MeshGenerator : MonoBehaviour {
     Vector2[] calculateCubicCurve(Node curveStart, Vector2 curvePoint1, Vector2 curvePoint2, Node curveEnd)
     {
         List<Vector2> points = new List<Vector2>();
-       /* Vector2 curvePoint1;
-        Vector2 curvePoint2;
-        if (curveStart.position.y > curveEnd.position.y)
-        {
-            curvePoint1 = new Vector2(curveEnd.position.x - 0.5f, curveStart.position.y);
-            curvePoint2 = new Vector2(curveStart.position.x + 0.5f, curveEnd.position.y);
-        } else
-        {
-            curvePoint1 = new Vector2(curveEnd.position.x - 0.5f, curveStart.position.y);
-            curvePoint2 = new Vector2(curveStart.position.x + 0.5f, curveEnd.position.y);
-        }
-        */
         for (float i = 0; i <= 1.01f; i += 0.1f)
         {
             points.Add(CubicCurve(curveStart.position,curvePoint1, curvePoint2, curveEnd.position,i));
@@ -251,11 +323,6 @@ public class MeshGenerator : MonoBehaviour {
     Vector2 Lerp(Vector2 start, Vector2 end, float pointPosition)
     {
         return start + (end - start) * pointPosition;
-    }
-
-    void CurvedMesh(Vector2[] points)
-    {
-
     }
 
     private void AssignNodes(Square[,] section)
