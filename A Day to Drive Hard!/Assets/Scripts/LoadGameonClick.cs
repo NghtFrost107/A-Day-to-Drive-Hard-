247daede4b7869e9d4965270382f96cbe0ac2c72
Mitchell <mitch.mcd18@gmail.com>
2018-09-08 15:44:06 +1200

    Moved UI files into main project directory

---
+++ A Day to Drive Hard!/Assets/Scripts/LoadGameonClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameonClick : MonoBehaviour {
+
+    public void LoadSceneOnCick(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
