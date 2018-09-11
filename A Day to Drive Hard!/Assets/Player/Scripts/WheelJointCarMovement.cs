4e7ada3548a8cec68aeda3cc8d646b3feac909c5
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2018-09-11 13:35:42 +1200

    Editted friction, resized tires, changed mass.
    Basically made the car better at handling the steeps slops by making it a two-wheel drive.

--- A Day to Drive Hard!/Assets/Player/Scripts/WheelJointCarMovement.cs
+++ A Day to Drive Hard!/Assets/Player/Scripts/WheelJointCarMovement.cs
@@ -15,13 +15,13 @@ public class WheelJointCarMovement : MonoBehaviour {
     //input for rotation of the car
     float torqueDir = 0f;
     //max fwd speed which the car can move at
-    float maxFwdSpeed = -3500;
+    float maxFwdSpeed = -8000;
     //max bwd speed
     float maxBwdSpeed = 1000f;
     //the rate at which the car accelerates
-    float accelerationRate = 6000;
+    float accelerationRate = 1000;
     //the rate at which car decelerates
-    float decelerationRate = -500;
+    float decelerationRate = -100;
     //how soon the car stops on braking
     float brakeSpeed = 2500f;
     //acceleration due to gravity
@@ -100,6 +100,7 @@ public class WheelJointCarMovement : MonoBehaviour {
         }
         //connect the motor to the joint
         wheelJoints[0].motor = motorBack;
+        wheelJoints[1].motor = motorBack;
 
     }
 }
