c061d853fac23abb9e263598e48d9c92a2cc11b3
NghtFrost107 <hbn8839@autuni.ac.nz>
2018-09-16 19:13:07 +1200

    back ;)

---
+++ GD2DH Game Menu/Assets/UpgradeMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeMenu : MonoBehaviour
+{
+
+    public void UpgradeHealth()
+    {
+        //get the player stats for health and add 1
+    }
+
+    public void UpgradeSpeed()
+    {
+        //get player speed and add to the speed to increase
+    }
+
+    public void UpgradeGrip()
+    {
+        //get player stats for the grip and increase
+    }
+
+
+
+}
