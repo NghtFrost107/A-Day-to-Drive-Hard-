ac5a929179842f17b95e392519929387ed1a7f9a
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2018-09-17 19:05:40 +1200

    Added Player folder back into Assets folder.

---
+++ A Day to Drive Hard!/Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public int playerHealth;
+    private bool invincible;
+    
+
+	// Use this for initialization
+	void Start () {
+        playerHealth = 3;
+        invincible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerHealth <= 0)
+        {
+            Debug.Log("The Player has run out of lives and the game SHOULD end");
+        }
+	}
+
+    // What initially happens when the player collides with an object
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Player Collided with an obstacle");
+            playerHealth--;
+            SetInvincible();
+        }
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            Debug.Log("Player Collided with a pickup");
+        }
+    }
+
+    public void SetInvincible()
+    {
+        invincible = true;
+        Debug.Log("Invis");
+        CancelInvoke("SetDamageable");
+        Invoke("SetDamageable", 3);
+    }
+
+    void SetDamageable()
+    {
+        invincible = false;
+        Debug.Log("not invis");
+    }
+
+}
