094a849c59cf3b337492cf226a8727c8b8414811
NghtFrost107 <hbn8839@autuni.ac.nz>
2018-09-19 00:10:18 +1200

    Pause menu added
    Need help with the upgrade menu script pulling the health value from player properties script. Continue, restart and exit game buttons work in the pause menu.

---
+++ A Day to Drive Hard!/Assets/UI/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject Panel;
+
+    
+    int counter = 1;
+
+    public void showHidePanel()
+    {
+
+        counter++;
+        if (counter % 2 == 1)
+        {
+            Panel.gameObject.SetActive(false);
+        }
+        else
+        {
+            Panel.gameObject.SetActive(true);
+        }
+    }
+
+    public void onPauseMenuToggle(bool active)
+    {
+        //GetComponent<>
+    }
+
+}
--- A Day to Drive Hard!/Assets/UI/Scripts/UpgradeMenu.cs
+++ A Day to Drive Hard!/Assets/UI/Scripts/UpgradeMenu.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeMenu : MonoBehaviour
 {
+    private PlayerProperties stats;
+
+    [SerializeField]
+    private Text healthText;
+
+    [SerializeField]
+    private Text costText;
+
+    void UpdateValues()
+    {
+        healthText.text = "Health: " + stats.playerHealth.ToString();
+    }
 
     public void UpgradeHealth()
     {
         //get the player stats for health and add 1
+        stats.playerHealth = stats.playerHealth + 1;
+        UpdateValues();
     }
 
     public void UpgradeSpeed()
