42a0d201c0f7189bef53ec94ef1e0adf7ad43db6
NghtFrost107 <hbn8839@autuni.ac.nz>
2018-10-10 10:11:12 +1300

    added setting menu for the pause menu
    creating the sliders is still in progress. As currently you need to press the buttons twice for it to work and i cant fix it

---
+++ A Day to Drive Hard!/Assets/UI/Scripts/InGameSettingMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameSettingMenu : MonoBehaviour {
+
+    public GameObject panel;
+
+    //counter is set to 1 so the panel is not displayed
+    int counter = 1;
+
+    //function is used to display the pause menu when the pause button is pushed
+    public void showHidePanel()
+    {
+
+        counter++;
+
+        //if the number is odd dont display the pause menu - if even display menu
+        if (counter % 2 == 1)
+        {
+            panel.gameObject.SetActive(false);
+        }
+        else
+        {
+            panel.gameObject.SetActive(true);
+        }
+    }
+}
