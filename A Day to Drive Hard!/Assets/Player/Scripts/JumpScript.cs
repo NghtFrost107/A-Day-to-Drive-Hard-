ea8a56472d548eed64554d1254a91aefc2889218
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2018-10-11 17:52:54 +1300

    Car can jump, just press 'j'

--- A Day to Drive Hard!/Assets/Player/Scripts/JumpScript.cs
+++ A Day to Drive Hard!/Assets/Player/Scripts/JumpScript.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpScript : MonoBehaviour {
+public class JumpScript : MonoBehaviour
+{
+    private bool canJump = true;
+    private float jumpStart = 0f;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+    {
+        if(canJump)
+        {
+            if (Input.GetKeyDown("j"))
+            {
+                //transform.Translate(Vector3.up * 200 * Time.deltaTime, Space.World);
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(15, 35f);
+                canJump = false;
+
+                Invoke("jump", 2);
+            }
+        }
 	}
+
+    public void jump()
+    {
+        canJump = true;
+    }
 }
