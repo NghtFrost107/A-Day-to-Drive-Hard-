80aa00e79710b87eed2b6c23e9af32bcd625b679
Ravmyster1121 <rhninety@gmail.com>
2018-10-22 13:55:00 +1300

    Added a heart icons representing lives

---
+++ A Day to Drive Hard!/Assets/HeartHUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartHUD : MonoBehaviour {
+    public Sprite[] HeartSprites;
+    public Image HeartUI;
+    public Player player;
+    private Database database;
+
+    private void Awake()
+    {
+        database = GameObject.FindGameObjectWithTag("Database").GetComponent<Database>();
+    }
+
+    private void Update()
+    {
+        try
+        {
+            HeartUI.sprite = HeartSprites[database.player.playerHealth];
+        }
+        catch (System.IndexOutOfRangeException e)
+        {
+
+        }
+        
+    }
+}
