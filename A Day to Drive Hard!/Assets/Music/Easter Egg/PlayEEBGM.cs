1fc204c923e55424f71795af6f13b4e7bf2bf0a8
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2018-10-23 18:57:24 +1300

    Added music to the Main Menu, In Game and Easter Egg. Only one song for each. Main Menu and In Game Music loops.

---
+++ A Day to Drive Hard!/Assets/Music/Easter Egg/PlayEEBGM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEEBGM : MonoBehaviour {
+
+    public GameObject carModel;
+    public float positionY;
+
+	// Use this for initialization
+	void Start ()
+    {
+        positionY = 0.0f;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionY = carModel.transform.position.y;
+
+        if (!EEBGM.Instance().gameObject.GetComponent<AudioSource>().isPlaying && positionY < -45)
+        {
+            MMBGM.Instance().gameObject.GetComponent<AudioSource>().Stop();
+            GPBGM.Instance().gameObject.GetComponent<AudioSource>().Stop();
+            EEBGM.Instance().gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ A Day to Drive Hard!/Assets/Music/Main Menu/PlayMMBGM.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMMBGM : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start ()
+    {
+        if (!MMBGM.Instance().gameObject.GetComponent<AudioSource>().isPlaying)
+        {
+            MMBGM.Instance().gameObject.GetComponent<AudioSource>().Play();
+            GPBGM.Instance().gameObject.GetComponent<AudioSource>().Stop();
+            EEBGM.Instance().gameObject.GetComponent<AudioSource>().Stop();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+}
