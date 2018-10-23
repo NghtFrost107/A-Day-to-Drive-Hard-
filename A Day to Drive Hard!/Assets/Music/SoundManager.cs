7a0ebb291aeda2575af0d754b3c4594324caa22d
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2018-10-23 21:50:54 +1300

    Update into FixedUpdate. In Game Sliders work now.

--- A Day to Drive Hard!/Assets/Music/SoundManager.cs
+++ A Day to Drive Hard!/Assets/Music/SoundManager.cs
@@ -7,9 +7,9 @@ public class SoundManager : MonoBehaviour {
 
     public AudioSource carEngine;                   //Drag a reference to the audio source which will play the sound effects.
     public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
-    public Slider carEngineSlide;
-    public Slider musicSlide;
-    public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.             
+    public static Slider carEngineSlide;
+    public static Slider musicSlide;
+    public static SoundManager instance;     //Allows other scripts to call functions from SoundManager.             
     
 
 
@@ -17,19 +17,23 @@ public class SoundManager : MonoBehaviour {
     {
         //Check if there is already an instance of SoundManager
         if (instance == null)
+        {
             //if not, set it to this.
             instance = this;
+        }
         //If instance already exists:
         else if (instance != this)
+        {
             //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
             Destroy(gameObject);
+        }
 
         //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
         DontDestroyOnLoad(gameObject);
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
         carEngine.volume = carEngineSlide.value;
         musicSource.volume = musicSlide.value;
